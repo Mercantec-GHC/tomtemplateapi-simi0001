16fa4f9d293af63ba0ada38ae093f11516d253c4
sila1400 <stilhofsvej46@gmail.com>
2024-08-06 14:31:48 +0200

    Api showing data

---
+++ Benzin/Benzin/Data/PriceData.cs
@@ -0,0 +1,18 @@
+﻿namespace Benzin.Data
+{
+    
+    public class PriceData
+    {
+       
+        static async Task Main(string[] args)
+        {
+            string apiUrl = "https://magsapi.onrender.com/api/miles95";
+        }
+    }
+    public class Data
+    {
+        public string Price { get; set; }
+        public string Date { get; set; }
+    }
+    
+}
--- Benzin/Benzin/Program.cs
+++ Benzin/Benzin/Program.cs
@@ -2,13 +2,14 @@ using Benzin.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,3 +30,4 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
